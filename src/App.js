187314651ec1572a7c2e4bfe2a5ad3b669b00b7f
Lucas Aramendy <yataylucas@gmail.com>
2023-10-27 12:37:41 -0300

    Change background-color

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link" 
           href="https://www.propelleraero.com/dirtmate/" 
           target="_blank"
           rel="noopener noreferrer"
